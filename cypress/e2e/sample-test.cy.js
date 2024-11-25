badf655c3850723eba5113788627ad60239d19e2
premsai131 <98572257+premsai131@users.noreply.github.com>
2024-11-25 20:46:36 +0530

    Create sample-test.cy.js

---
+++ cypress/e2e/sample-test.cy.js
@@ -0,0 +1,9 @@
+describe('Sample Test', () => {
+    it('Visits the Cypress example site', () => {
+      cy.visit('https://example.cypress.io');
+      cy.contains('type').click();
+      cy.url().should('include', '/commands/actions');
+      cy.get('.action-email').type('test@example.com').should('have.value', 'test@example.com');
+    });
+  });
+  
