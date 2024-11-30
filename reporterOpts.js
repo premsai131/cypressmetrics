de727f982a13e2a5cdb0496350f01ca2065ecfdc
premsai131 <98572257+premsai131@users.noreply.github.com>
2024-11-30 23:52:51 +0530

    Update reporterOpts.js

--- reporterOpts.js
+++ reporterOpts.js
@@ -1,16 +1,17 @@
-const testType = process.env.TEST_TYPE
+const testType = process.env.TEST_TYPE || 'DefaultTestType'; // Default to avoid undefined
+const matrixContainer = process.env.MATRIX_CONTAINER || 'default'; // Add MATRIX_CONTAINER
+
 module.exports = {
-    "reporterEnabled": "cypress-mochawesome-reporter",
-    "cypressMochawesomeReporterReporterOptions": {
-        "reportDir": `cypress/reports/mocha/Regression`,
-        "reportFilename": "report",
-        "embeddedScreenshots": false,
-        "ignoreVideos":true,
-        "inlineAssets": false,
-        "quiet": true,
-        "overwrite": false,
-        "html": false,
-        "saveJson": true
-      }
-}
-  
+  reporterEnabled: "cypress-mochawesome-reporter",
+  cypressMochawesomeReporterReporterOptions: {
+    reportDir: `cypress/reports/mocha/Regression-${matrixContainer}`, // Unique directory
+    reportFilename: "report",
+    embeddedScreenshots: false,
+    ignoreVideos: true,
+    inlineAssets: false,
+    quiet: true,
+    overwrite: false,
+    html: false,
+    saveJson: true,
+  },
+};
