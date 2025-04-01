cc3712ad140cff61731f694653b9631e17668d7d
premsai131 <98572257+premsai131@users.noreply.github.com>
2025-04-01 14:09:52 +0530

    Update parseTestReports.ts

--- scripts/parseTestReports.ts
+++ scripts/parseTestReports.ts
@@ -3,14 +3,14 @@ import * as path from "path";
 
 // Resolving paths to the root of the project
 const REPORT_DIR = path.resolve("report");  // Report folder is at the root of the project
-const OUTPUT_JSON_FILE = path.resolve("../test_results.json");  // Default output file at the root level
+const OUTPUT_JSON_FILE = path.resolve("test_results.json");  // Default output file at the root level
 
 // Get the container name from an environment variable
 const MATRIX_CONTAINER = process.env.MATRIX_CONTAINER || ""; 
 
 // If MATRIX_CONTAINER is set, generate a unique file name
 const MATRIX_OUTPUT_JSON_FILE = MATRIX_CONTAINER
-  ? path.resolve(`../test_results_${MATRIX_CONTAINER}.json`)  // Writes directly to the root level
+  ? path.resolve(`test_results_${MATRIX_CONTAINER}.json`)  // Writes directly to the root level
   : OUTPUT_JSON_FILE;
 
 async function getAllJsonFilesAsync(dir: string): Promise<string[]> {
